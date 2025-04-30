40ca9d75d12c35b97c09011566b5a4ad43eb99c5
margoxt <angelicatinio14@gmail.com>
2025-04-30 19:26:24 +0800

    enabling killzone-height
    Editing the Killzone node so that when the player goes to the void he will die and respawn within 0.6 seconds

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,1 +1,10 @@
 extends Area2D
+
+@onready var timer: Timer = $Timer 	#Hold and drag timer node
+
+func _on_body_entered(body) -> void:
+	print("You died!")
+	timer.start()		#The timer starts now
+
+func _on_timer_timeout() -> void:	#And this code runs when the timer ends
+	get_tree().reload_current_scene()	#Reloads and restarts the game
